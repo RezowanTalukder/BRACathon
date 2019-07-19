253b142797d83f5eb305e1cc4ccf68aa4b19dbd0
Rezowan Talukder <35560302+RezowanTalukder@users.noreply.github.com>
2019-07-19 17:12:32 +0600

    Add files via upload

--- map_script.js
+++ map_script.js
@@ -21,21 +21,53 @@ function initMap() {
       info:
         '<strong>Mirpur Branch 3</strong><br>\r\
           Md Tanvir Rahman<br> Money Collector<br>Performance: 90%<br>\
-          <a href="https://goo.gl/maps/QGUrqZPsYp92">Get Directions</a>',
+          <a href="bonogram.html">Get Directions</a>',
           lat: 23.1397,
           long: 90.594,
     }
+
+    var broadway2 = {
+      info:
+        '<strong>Chipotle on Broadway</strong><br>\
+          5224 N Broadway St<br> Chicago, IL 60640<br>\
+          <a href="https://goo.gl/maps/jKNEDz4SyyH2"><b>Get Directions</b></a>',
+      lat: 23.3397,
+      long: 90.194,
+    }
+  
+    var belmont2 = {
+      info:
+        '<strong>Chipotle on Belmont</strong><br>\
+          1025 W Belmont Ave<br> Chicago, IL 60657<br>\
+          <a href="https://goo.gl/maps/PHfsWTvgKa92">Get Directions</a>',
+          lat: 23.0197,
+          long: 90.494,
+    }
+  
+    var sheridan2 = {
+      info:
+        '<strong>Mirpur Branch 3</strong><br>\r\
+          Md Tanvir Rahman<br> Money Collector<br>Performance: 90%<br>\
+          <a href="bonogram.html">Get Directions</a>',
+          lat: 23.1697,
+          long: 90.5194,
+    }
+
   
     var locations = [
       [broadway.info, broadway.lat, broadway.long, 0],
       [belmont.info, belmont.lat, belmont.long, 1],
       [sheridan.info, sheridan.lat, sheridan.long, 2],
+      [broadway2.info, broadway2.lat, broadway2.long, 3],
+      [belmont2.info, belmont2.lat, belmont2.long, 4],
+      [sheridan2.info, sheridan2.lat, sheridan2.long, 5],
     ]
   
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 10,
       center: new google.maps.LatLng(23.1397, 90.594),
       mapTypeId: google.maps.MapTypeId.ROADMAP,
+      icon:'icon.png'
     })
   
     var infowindow = new google.maps.InfoWindow({})
